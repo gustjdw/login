d0f7fefaf527d703f9b48e52c06070c5ce461d66
gustjdw <johs710@gachon.ac.kr>
2023-03-09 14:47:08 +0900

    로그인 처리

---
+++ src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,6 @@
+package hello.login.web;
+
+//생성하지 않고 상수만 갖다 쓸거라 인터페이스로 구현하는게 나음
+public class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
---
+++ src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,35 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void sessionTest() {
+
+        //Session 생성 (서버 -> 클라이언트)
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        //요청에 응답 쿠키 저장 (클라이언트 -> 서버)
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        //Session 조회
+        Object result = sessionManager.getSession(request);
+        assertThat(result).isEqualTo(member);
+        
+        //Session 만료
+        sessionManager.expire(request);
+        Object expired = sessionManager.getSession(request);
+        assertThat(expired).isNull();
+    }
+}
